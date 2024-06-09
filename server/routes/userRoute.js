ed6b0b04357948a097fd2e0deece4f63f04d8d62
Sanjay-2004 <sanjay.agamamidi@gmail.com>
2024-06-09 15:32:24 +0530

    performance from backend

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,28 +1,27 @@
-import mongoose from "mongoose";
-import express, { response } from 'express';
-import User from '../models/user.js'
+import express from "express";
+import User from "../models/user.js";
 
 const router = express.Router();
 
 router.route("/").post(async (req, res) => {
-    try {
-        console.log("Entered Here");
-        console.log(req.body);
-        const {userEmail, userName, userId} = req.body;
-        console.log(userEmail,userName,userId);
-        const user = await User.findOne({userID: userId});
-        if(!user){
-            await new User({userName,userEmail,userId}).save();
-            return res.json({"status":"User created."}).status(201);
-        }else return res.json({"status":"User found."}).status(200);
-    } catch (error) {
-        console.log(error);
-        return res.json({"status":"There was a problem."}).status(500);
-    }
-})
+  try {
+    console.log("Entered Here");
+    console.log(req.body);
+    const { userEmail, userName, userId } = req.body;
+    console.log(userEmail, userName, userId);
+    const user = await User.findOne({ userID: userId });
+    if (!user) {
+      await new User({ userName, userEmail, userId }).save();
+      return res.json({ status: "User created." }).status(201);
+    } else return res.json({ status: "User found." }).status(200);
+  } catch (error) {
+    console.log(error);
+    return res.json({ status: "There was a problem." }).status(500);
+  }
+});
 
-router.route("/").get(async (req,res)=>{
-    return res.json({"status":"Workisdfsfdsfsdng"}).status(200);
-})
+router.route("/").get(async (req, res) => {
+  return res.json({ status: "Workisdfsfdsfsdng" }).status(200);
+});
 
 export default router;
