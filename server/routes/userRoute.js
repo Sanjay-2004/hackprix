504aca916fdf31f28fad508fc8daea60d18437a0
sudeepgud <sudeepgudipalli@gmail.com>
2024-06-09 12:59:16 +0530

    user model updates

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import express from 'express';
+import express, { response } from 'express';
 import User from '../models/user.js'
 
 const router = express.Router();
@@ -7,10 +7,20 @@ const router = express.Router();
 router.route("/").post(async (req, res) => {
     try {
         console.log("Entered Here");
-        const {userEmail, userName} = req.body;
-
-
+        const {userEmail, userName, userID} = req.body;
+        const user = await User.findOne({userID: userID});
+        if(!user){
+            await new User({userName,userEmail,userID}).save();
+            return res.json({"status":"User created."}).status(201);
+        }else return res.json({"status":"User found."}).status(200);
     } catch (error) {
-        
+        console.log(error);
+        return res.json({"status":"There was a problem."}).status(500);
     }
 })
+
+router.route("/").get(async (req,res)=>{
+    return res.json({"status":"Workisdfsfdsfsdng"}).status(200);
+})
+
+export default router;
